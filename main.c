42d35ec3b05c2af0e8317be6e8d644b99d0d7dd9
Vasileios Vrouliotis <vvrouliotis@gmail.com>
2019-12-24 15:48:48 +0200

    Sprite animation

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "Knights.c"
 
-void main() {
-    printf("Hello World");
+void main() 
+{
+    UINT8 currentSpriteIndex = 0;
+    set_sprite_data(0, 2, Knights);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 88, 78);
+    SHOW_SPRITES;
+
+    while(1)
+    {
+        if (currentSpriteIndex == 0) currentSpriteIndex = 1;
+        else currentSpriteIndex = 0;
+        set_sprite_tile(0, currentSpriteIndex);
+        delay(1000);
+    }
+    
 }
