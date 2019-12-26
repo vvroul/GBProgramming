e8b139fd7979e2ff2f4f86fc27386b3de854dc66
Vasileios Vrouliotis <vvrouliotis@gmail.com>
2019-12-26 13:17:27 +0200

    Add background

--- main.c
+++ main.c
@@ -1,33 +1,20 @@
 #include <gb/gb.h>
 #include <stdio.h>
 #include "Knights.c"
+#include "backgroundtiles.c"
+#include "simplebg_map.c"
 
 void main() 
-{
-    UINT8 currentSpriteIndex = 0;
-    set_sprite_data(0, 2, Knights);
-    set_sprite_tile(0, 0);
-    move_sprite(0, 88, 78);
-    SHOW_SPRITES;
+{   
+    set_bkg_data(0, 7, TileLabel);
+    set_bkg_tiles(0, 0, 40, 18, simplebg_map);
 
-    while(1)
+    SHOW_BKG;
+    DISPLAY_ON;
+
+    while (1) 
     {
-        switch (joypad())
-        {
-            case J_LEFT:
-                scroll_sprite(0, -1, 0);
-                break;
-            case J_RIGHT:
-                scroll_sprite(0, 1, 0);
-                break;
-            case J_UP:
-                scroll_sprite(0, 0, -1);
-                break;
-            case J_DOWN:
-                scroll_sprite(0, 0, 1);
-                break;
-        }
+        scroll_bkg(1, 0);
         delay(50);
     }
-    
 }
