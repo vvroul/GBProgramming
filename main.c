66607fa8f96205f55077edda366fb1d3d8b00a0a
Vasileios Vrouliotis <vvrouliotis@gmail.com>
2019-12-31 15:20:21 +0200

    Add jump to sprite

--- main.c
+++ main.c
@@ -6,23 +6,88 @@
 #include "simplebg_map.c"
 #include "windowmap.c"
 
+INT8 playerLocation[2];
+BYTE isJumping;
+UINT8 gravity = -2;
+UINT8 currentSpeedY;
+UINT8 floorYPosition = 100;
+
+void performantDelay(UINT8 numloops)
+{
+    UINT8 i;
+    for(i = 0; i < numloops; i++)
+    {
+        wait_vbl_done();
+    }
+}
+
+INT8 wouldHitSurface(UINT8 projectedYPosition)
+{
+    if (projectedYPosition >= floorYPosition)
+    {
+        return floorYPosition;
+    }
+    return -1;
+}
+
+void jump(UINT8 spriteId, UINT8 spriteLocation[2])
+{
+    INT8 possibleSurfaceY;
+
+    if (isJumping == 0) 
+    {
+        isJumping = 1;
+        currentSpeedY = 10;
+    }
+
+    currentSpeedY += gravity;
+    spriteLocation[1] -= currentSpeedY;
+    possibleSurfaceY = wouldHitSurface(spriteLocation[1]);
+
+    if (possibleSurfaceY > -1)
+    {
+        isJumping = 0;
+        move_sprite(spriteId, spriteLocation[0], possibleSurfaceY);
+    }
+    else
+    {
+        move_sprite(spriteId, spriteLocation[0], spriteLocation[1]);
+    }
+}
+
 void main() 
 {   
-    NR52_REG = 0x80;
-    NR50_REG = 0x77;
-    NR51_REG = 0xFF;
+    set_sprite_data(0, 8, Knights);
+    set_sprite_tile(0, 0);
 
-    while (1) 
+    playerLocation[0] = 10;
+    playerLocation[1] = floorYPosition;
+    isJumping = 0;
+    move_sprite(0, playerLocation[0], playerLocation[1]);
+
+    DISPLAY_ON;
+    SHOW_SPRITES;
+
+    while(1)
     {
-        UBYTE joypad_state = joypad();
-        if (joypad_state)
+        if (joypad() & J_LEFT) 
         {
-            NR41_REG = 0x1F;
-            NR42_REG = 0xF1;
-            NR43_REG = 0x30;
-            NR44_REG = 0xC0;
-            // NR14_REG = 0xC3;
-            delay(1000);
+            playerLocation[0] -= 2;
+            move_sprite(0, playerLocation[0], playerLocation[1]);
         }
+        
+        if (joypad() & J_RIGHT)
+        {
+            playerLocation[0] += 2;
+            move_sprite(0, playerLocation[0], playerLocation[1]);
+        }
+
+        if ((joypad() & J_A) || (isJumping == 1))
+        {
+            jump(0, playerLocation);
+        }
+
+        performantDelay(4);
     }
+    
 }
