2b992373d8982f4f2393d1cb0fe367c1f7ef1e0e
Vrouliotis Vasileios <vvrouliotis@gmail.com>
2020-01-02 19:45:21 +0200

    Add meta sprites

--- main.c
+++ main.c
@@ -5,12 +5,59 @@
 #include "backgroundtiles.c"
 #include "simplebg_map.c"
 #include "windowmap.c"
+#include "GameCharacter.c"
+#include "GameSprites.c"
 
-INT8 playerLocation[2];
-BYTE isJumping;
-UINT8 gravity = -2;
-UINT8 currentSpeedY;
-UINT8 floorYPosition = 100;
+struct GameCharacter ship;
+struct GameCharacter buggy;
+UBYTE spriteSize = 8;
+
+void moveGameCharacter(struct GameCharacter* character, UINT8 x, UINT8 y)
+{
+    move_sprite(character->spritids[0], x, y);
+    move_sprite(character->spritids[1], x + spriteSize, y);
+    move_sprite(character->spritids[2], x, y + spriteSize);
+    move_sprite(character->spritids[3], x + spriteSize, y + spriteSize);
+}
+
+
+void setupShip()
+{
+    ship.x = 80;
+    ship.y = 130;
+    ship.width = 16;
+    ship.height = 16;
+
+    set_sprite_tile(0, 0);
+    ship.spritids[0] = 0;
+    set_sprite_tile(1,1);
+    ship.spritids[1] = 1;
+    set_sprite_tile(2, 2);
+    ship.spritids[2] = 2;
+    set_sprite_tile(3, 3);
+    ship.spritids[3] = 3;
+
+    moveGameCharacter(&ship, ship.x, ship.y);
+}
+
+void setupBug()
+{
+    buggy.x = 30;
+    buggy.y = 0;
+    buggy.width = 16;
+    buggy.height = 16;
+
+    set_sprite_tile(4, 4);
+    buggy.spritids[0] = 4;
+    set_sprite_tile(5, 5);
+    buggy.spritids[1] = 5;
+    set_sprite_tile(6, 6);
+    buggy.spritids[2] = 6;
+    set_sprite_tile(7, 7);
+    buggy.spritids[3] = 7;
+
+    moveGameCharacter(&buggy, buggy.x, buggy.y);
+}
 
 void performantDelay(UINT8 numloops)
 {
@@ -21,73 +68,40 @@ void performantDelay(UINT8 numloops)
     }
 }
 
-INT8 wouldHitSurface(UINT8 projectedYPosition)
-{
-    if (projectedYPosition >= floorYPosition)
-    {
-        return floorYPosition;
-    }
-    return -1;
-}
-
-void jump(UINT8 spriteId, UINT8 spriteLocation[2])
-{
-    INT8 possibleSurfaceY;
-
-    if (isJumping == 0) 
-    {
-        isJumping = 1;
-        currentSpeedY = 10;
-    }
-
-    currentSpeedY += gravity;
-    spriteLocation[1] -= currentSpeedY;
-    possibleSurfaceY = wouldHitSurface(spriteLocation[1]);
-
-    if (possibleSurfaceY > -1)
-    {
-        isJumping = 0;
-        move_sprite(spriteId, spriteLocation[0], possibleSurfaceY);
-    }
-    else
-    {
-        move_sprite(spriteId, spriteLocation[0], spriteLocation[1]);
-    }
-}
-
 void main() 
-{   
-    set_sprite_data(0, 8, Knights);
-    set_sprite_tile(0, 0);
+{
 
-    playerLocation[0] = 10;
-    playerLocation[1] = floorYPosition;
-    isJumping = 0;
-    move_sprite(0, playerLocation[0], playerLocation[1]);
+    set_sprite_data(0, 8, GameSprites);
 
-    DISPLAY_ON;
+    setupShip();
+    setupBug();
+
     SHOW_SPRITES;
+    DISPLAY_ON;
 
     while(1)
     {
-        if (joypad() & J_LEFT) 
+        if (joypad() & J_LEFT)
         {
-            playerLocation[0] -= 2;
-            move_sprite(0, playerLocation[0], playerLocation[1]);
+            ship.x -= 2;
+            moveGameCharacter(&ship, ship.x, ship.y);
         }
-        
         if (joypad() & J_RIGHT)
         {
-            playerLocation[0] += 2;
-            move_sprite(0, playerLocation[0], playerLocation[1]);
+            ship.x += 2;
+            moveGameCharacter(&ship, ship.x, ship.y);
         }
 
-        if ((joypad() & J_A) || (isJumping == 1))
+        //make the bug move down the screen
+        buggy.y += 5;
+        if (buggy.y >= 144)
         {
-            jump(0, playerLocation);
+            buggy.y = 0;
+            buggy.x = ship.x;
         }
+        moveGameCharacter(&buggy, buggy.x, buggy.y);
 
-        performantDelay(4);
+        performantDelay(5)
     }
     
 }
